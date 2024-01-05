b875c8857562c51a5776095a5a31c79343c5d068
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-05 06:48:20 +0000

    Release 0.1.3

--- src/scorecard/client.py
+++ src/scorecard/client.py
@@ -3,10 +3,8 @@
 import typing
 
 import httpx
-import os
 
 from .core.client_wrapper import AsyncClientWrapper, SyncClientWrapper
-from .core.api_error import ApiError
 from .environment import ScorecardEnvironment
 from .resources.run.client import AsyncRunClient, RunClient
 from .resources.testcase.client import AsyncTestcaseClient, TestcaseClient
@@ -20,27 +18,10 @@ class Scorecard:
         *,
         base_url: typing.Optional[str] = None,
         environment: ScorecardEnvironment = ScorecardEnvironment.DEFAULT,
-        api_key: typing.Optional[str] = os.getenv("SCORECARD_API_KEY"),
+        api_key: str,
         timeout: typing.Optional[float] = 60,
         httpx_client: typing.Optional[httpx.Client] = None
     ):
-        """
-        Instantiate the Scorecard SDK.
-
-        Parameters:
-            - base_url: typing.Optional[str]. Overrides the base_url used by the client
-
-            - environment: typing.Optional[ScorecardEnvironment]. Defaults to https://api.getscorecard.ai.
-
-            - api_key: typing.Optional[str]. Your Scorecard API Key. Defaults to the env variable SCORECARD_API_KEY. 
-
-            - timeout: typing.Optional[float]. Defaults to 60 seconds. 
-
-            - httpx_client: typing.Optional[httpx.Client]. Override the httpx client used by the sdk. 
-        """
-        if api_key is None: 
-            raise ApiError(
-                body="Please provide an api_key or set SCORECARD_API_KEY")
         self._client_wrapper = SyncClientWrapper(
             base_url=_get_base_url(base_url=base_url, environment=environment),
             api_key=api_key,
@@ -48,8 +29,9 @@ class Scorecard:
         )
         self.testset = TestsetClient(client_wrapper=self._client_wrapper)
         self.testcase = TestcaseClient(client_wrapper=self._client_wrapper)
-        self.run = RunClient(client_wrapper=self._client_wrapper)
         self.testrecord = TestrecordClient(client_wrapper=self._client_wrapper)
+        self.run = RunClient(client_wrapper=self._client_wrapper)
+
 
 class AsyncScorecard:
     def __init__(
@@ -61,23 +43,6 @@ class AsyncScorecard:
         timeout: typing.Optional[float] = 60,
         httpx_client: typing.Optional[httpx.AsyncClient] = None
     ):
-        """
-        Instantiate the Scorecard SDK.
-
-        Parameters:
-            - base_url: typing.Optional[str]. Overrides the base_url used by the client
-
-            - environment: typing.Optional[ScorecardEnvironment]. Defaults to https://api.getscorecard.ai.
-
-            - api_key: typing.Optional[str]. Your Scorecard API Key. Defaults to the env variable SCORECARD_API_KEY. 
-
-            - timeout: typing.Optional[float]. Defaults to 60 seconds. 
-
-            - httpx_client: typing.Optional[httpx.AsyncClient]. Override the httpx client used by the sdk. 
-        """
-        if api_key is None: 
-            raise ApiError(
-                body="Please provide an api_key or set SCORECARD_API_KEY")
         self._client_wrapper = AsyncClientWrapper(
             base_url=_get_base_url(base_url=base_url, environment=environment),
             api_key=api_key,
@@ -85,8 +50,8 @@ class AsyncScorecard:
         )
         self.testset = AsyncTestsetClient(client_wrapper=self._client_wrapper)
         self.testcase = AsyncTestcaseClient(client_wrapper=self._client_wrapper)
-        self.run = AsyncRunClient(client_wrapper=self._client_wrapper)
         self.testrecord = AsyncTestrecordClient(client_wrapper=self._client_wrapper)
+        self.run = AsyncRunClient(client_wrapper=self._client_wrapper)
 
 
 def _get_base_url(*, base_url: typing.Optional[str] = None, environment: ScorecardEnvironment) -> str:
--- src/scorecard/core/client_wrapper.py
+++ src/scorecard/core/client_wrapper.py
@@ -13,8 +13,8 @@ class BaseClientWrapper:
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
-            "X-Fern-SDK-Name": "scorecard",
-            "X-Fern-SDK-Version": "0.0.0",
+            "X-Fern-SDK-Name": "fern-scorecard",
+            "X-Fern-SDK-Version": "0.1.3",
         }
         headers["X-API-Key"] = self.api_key
         return headers
