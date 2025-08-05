16a56e4897eb4f33f4e0d929be4549eb0d239799
Vishal Devireddy <vishal@scorecard.io>
2025-08-04 20:07:29 -0700

    feat: Add multi-turn simulation helper

--- src/scorecard_ai/lib/__init__.py
+++ src/scorecard_ai/lib/__init__.py
@@ -2,3 +2,7 @@ from ._helpers import (
     run_and_evaluate as run_and_evaluate,
     async_run_and_evaluate as async_run_and_evaluate,
 )
+from ._multi_turn_simulation import (
+    ChatMessage as ChatMessage,
+    multi_turn_simulation as multi_turn_simulation,
+)
